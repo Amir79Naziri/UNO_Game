8779b6b58caf9597e1e6a8705ee32d237676f826
9726081 <9726081@ceit.local>
2020-04-10 23:19:59 +0430

    "Change in UserInterface class"

--- src/Reader.java
+++ src/Reader.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 public class Reader
 {
     private Scanner reader;
-    private Printer printer;
 
 
-    public Reader (Printer printer)
+    public Reader ()
     {
         reader = new Scanner (System.in);
-        this.printer = printer;
+
     }
 
-    public int getIndexOfChosenCard (Player playerInTurn)
+    public int getIndexOfChosenCard (Player playerInTurn, Printer printer)
     {
         if (playerInTurn == null)
             return -1;
         printer.printIndexGivingMassageFromPlayer ();
         int index = reader.nextInt () - 1;
         if (index >= playerInTurn.getCards ().size ())
-            return getIndexOfChosenCard (playerInTurn);
+            return getIndexOfChosenCard (playerInTurn,printer);
         else
             return index;
     }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,4 +1,30 @@
+import java.util.LinkedList;
+
 public class UserInterface
 {
+    private Printer printer;
+    private Reader reader;
+
+    public UserInterface ()
+    {
+        printer = new Printer ();
+        reader = new Reader ();
+    }
+
+    public void showForHumanPlayer (Board board, Player playerInTurn)
+    {
+        if (playerInTurn == null)
+            return;
+        printer.printCardOnBoard (board);
+        printer.printCardsOfPlayer (playerInTurn.getCards ());
+        reader.getIndexOfChosenCard (playerInTurn,printer);
+    }
+
+    public void showForMachinePlayer (Board board, Player playerInTurn)
+    {
+        if (playerInTurn == null)
+            return;
+        printer.printCardOnBoard (board);
+    }
 
 }
